48d7b1e82ddf8c5967253afe9bf10381c62b5738
Meredyk48 <brmeredyk@gmail.com>
2025-03-25 20:47:31 -0300

    Finalizando configurações iniciais gearvault

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Contato/static')
+    os.path.join(BASE_DIR, 'gearvault/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- gearvault/views.py
+++ gearvault/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import Produto
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+# @login_required(login_url='login')
+# def index(request):
+#     contatos = Contato.objects.all()
+#     categorias = Categoria.objects.all()
+#     return render(request, 'pages/index.html', {'contatos': contatos, 'categorias': categorias})
+
+
+# @login_required(login_url='login')
+# def contato_detalhes(request, id):
+# contato = Contato.objects.get(id=id)
+# , {'contato': contato})
+#   return render(request, 'pages/contato_detalhes.html', {'contato': Contato.objects.get(id=id)})
+
+
+def home(request):
+    produtos = Produto.objects.all()
+    return render(request, 'pages/home.html', {'produtos': produtos})
