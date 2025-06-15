991c161580e4ca9dc01c24aad42cc353e58762b9
Luiz Bernardi <luizbernardi37@gmail.com>
2025-06-15 20:12:25 -0300

    finalizando modelos praticamente

--- core/settings.py
+++ core/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files (uploads de imagens e outros arquivos)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
