b16bb3be02b5a46e1aefe34493d8c5de88ad3d0c
Luiz Bernardi <luizbernardi37@gmail.com>
2025-06-15 13:15:48 -0300

    adicionando side bar e esquema para adicionar os links das paginas

--- gearvault/views.py
+++ gearvault/views.py
@@ -1,36 +1,36 @@
 from django.shortcuts import render, redirect
 from .models import Produto
 from django.contrib.auth.decorators import login_required
-# Create your views here.
-
-
-# @login_required(login_url='login')
-# def index(request):
-#     contatos = Contato.objects.all()
-#     categorias = Categoria.objects.all()
-#     return render(request, 'pages/index.html', {'contatos': contatos, 'categorias': categorias})
-
-
-# @login_required(login_url='login')
-# def contato_detalhes(request, id):
-# contato = Contato.objects.get(id=id)
-# , {'contato': contato})
-#   return render(request, 'pages/contato_detalhes.html', {'contato': Contato.objects.get(id=id)})
 
 @login_required
 def usuario_painel(request):
     if request.user.profile.role != 'USUARIO':
-        # Se não for usuário comum, redireciona para o painel correto
         if request.user.profile.role == 'ADMIN':
             return redirect('admin_painel')
-        return redirect('login')  # Se não tiver role válida, vai para login
-    return render(request, 'pages/user/painel.html')
+        return redirect('login')
+
+    return render(request, 'pages/user/painel.html', {'sidebar_links': get_sidebar_links(request.user)})
 
 @login_required
 def admin_painel(request):
     if request.user.profile.role != 'ADMIN':
-        # Se não for admin, redireciona para o painel correto
         if request.user.profile.role == 'USUARIO':
             return redirect('usuario_painel')
-        return redirect('login')  # Se não tiver role válida, vai para login
-    return render(request, 'pages/admin/painel.html')
+        return redirect('login')
+
+    return render(request, 'pages/admin/painel.html', {'sidebar_links': get_sidebar_links(request.user)})
+
+    # TODO: Adicionar URLs correspondentes para os painéis de administrador e usuário aqui.
+def get_sidebar_links(user):
+    role = getattr(user.profile, 'role', None)
+    if role == 'ADMIN':
+        return [
+            {'url': '', 'label': 'Painel Administrador'},
+            {'url': '', 'label': 'Gerenciar Usuários'},
+        ]
+    elif role == 'USUARIO':
+        return [
+            {'url': '', 'label': 'Dashboard'},
+            {'url': '', 'label': 'Perfil'},
+        ]
+    return []
