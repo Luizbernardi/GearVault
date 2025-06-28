e8dacd9073ff9982587d713bae262c37aa9e023d
Luiz Bernardi <luizbernardi37@gmail.com>
2025-06-28 16:23:34 -0300

    Add gunicorn to requirements and fix fly.toml

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=c15g3am!_%_)8@w%za_lcg)-0+n*f%ck7!zvm_1(&wd(+akry'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
